a587b8ccff882eb916a7e0546a315cf013e1be70
Lubos Racansky <lubos.racansky@gmail.com>
2022-06-22 13:35:22 +0200

    Fix #18: Implement OnbordingProvider consent methods

--- enrollment-server-onboarding/src/main/java/com/wultra/app/onboardingserver/mock/MockOnboardingProvider.java
+++ enrollment-server-onboarding/src/main/java/com/wultra/app/onboardingserver/mock/MockOnboardingProvider.java
@@ -17,12 +17,13 @@
  */
 package com.wultra.app.onboardingserver.mock;
 
-import com.wultra.app.onboardingserver.provider.LookupUserRequest;
-import com.wultra.app.onboardingserver.provider.OnboardingProvider;
-import com.wultra.app.onboardingserver.provider.SendOtpCodeRequest;
+import com.wultra.app.onboardingserver.provider.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+import java.util.UUID;
 
 /**
  * Mock onboarding provider for testing purposes.
@@ -48,4 +49,34 @@ public class MockOnboardingProvider implements OnboardingProvider {
         logger.info("Send OTP code called, user ID: {}, OTP code: {}, resend: {}", userId, otpCode, resend);
     }
 
+    @Override
+    public String fetchConsent(final ConsentTextRequest request) {
+        final UUID processId = request.getProcessId();
+        final String userId = request.getUserId();
+        final String consentType = request.getConsentType();
+        final Locale locale = request.getLocale();
+        logger.info("Fetching consent for processId: {}, userId: {}, consentType: {}, locale: {}", processId, userId, consentType, locale);
+
+        return "<html><body>" +
+                "<h1>Lorem ipsum</h1>" +
+                "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam posuere lacus quis dolor. Nullam feugiat, turpis at pulvinar" +
+                " vulputate, erat libero tristique tellus, nec bibendum odio risus sit amet ante. Nullam at arcu a est sollicitudin euismod. " +
+                "Aliquam in lorem sit amet leo accumsan lacinia. Duis condimentum augue id magna semper rutrum. Sed convallis magna eu sem. " +
+                "Pellentesque arcu. Phasellus faucibus molestie nisl. Maecenas fermentum, sem in pharetra pellentesque, velit turpis volutpat " +
+                "ante, in pharetra metus odio a lectus. Nullam feugiat, turpis at pulvinar vulputate, erat libero tristique tellus, nec bibendum " +
+                "odio risus sit amet ante. Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, " +
+                "vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?" +
+                "</body></html>";
+    }
+
+    @Override
+    public ApproveConsentResponse approveConsent(final ApproveConsentRequest request) {
+        final UUID processId = request.getProcessId();
+        final String userId = request.getUserId();
+        final String consentType = request.getConsentType();
+        final Boolean approved = request.getApproved();
+
+        logger.info("Approved consent for processId: {}, userId: {}, consentType: {}, approved: {}", processId, userId, consentType, approved);
+        return new ApproveConsentResponse();
+    }
 }
