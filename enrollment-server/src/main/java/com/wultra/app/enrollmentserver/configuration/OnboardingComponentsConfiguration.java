dd358a24c307b0fd2566d25a47a1d03797d8504f
Roman Strobl <roman.strobl@wultra.com>
2022-09-06 18:55:15 +0200

    Increase activation exception handler order

--- enrollment-server/src/main/java/com/wultra/app/enrollmentserver/configuration/OnboardingComponentsConfiguration.java
+++ enrollment-server/src/main/java/com/wultra/app/enrollmentserver/configuration/OnboardingComponentsConfiguration.java
@@ -35,6 +35,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
@@ -172,6 +174,7 @@ public class OnboardingComponentsConfiguration {
      * @return Activation exception handler.
      */
     @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
     public ActivationExceptionHandler activationExceptionHandler(){
         return new ActivationExceptionHandler();
     }
